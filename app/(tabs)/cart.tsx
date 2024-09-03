f0ca5539b776b6fedd8db372b6bb6b8a8f3939b3
mahdimortuza <mahdi.webx@gmail.com>
2024-09-03 09:55:21 +0600

    checkout page added

--- app/(tabs)/cart.tsx
+++ app/(tabs)/cart.tsx
@@ -1,37 +1,50 @@
 import CartItem from "@/components/cart/cartItem";
 import PriceTable from "@/components/cart/priceTable";
 import { CartData } from "@/data/cartData";
+import { useRouter } from "expo-router";
 import { useState } from "react";
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function Cart() {
   const [cartItems, setCartItems] = useState(CartData);
+  const router = useRouter();
+
   return (
-    <View style={{}}>
-      <View>
-        <Text style={styles.heading}>
-          {cartItems?.length > 0
-            ? `You have ${cartItems.length} products in the cart`
-            : "Your cart is empty."}
-        </Text>
-        {cartItems?.length > 0 && (
-          <>
-            <ScrollView>
-              {cartItems?.map((item) => (
-                <CartItem key={item._id} item={item} />
-              ))}
-            </ScrollView>
-            <View>
-              <PriceTable title="Price" price={999} />
-              <PriceTable title="Tax" price={1} />
-              <PriceTable title="Shipping" price={1} />
-              <View style={styles.grandTotal}>
-                <PriceTable title="Grand total" price={1001} />
-              </View>
+    <View>
+      <Text style={styles.heading}>
+        {cartItems?.length > 0
+          ? `You have ${cartItems.length} products in the cart`
+          : "Your cart is empty."}
+      </Text>
+      {cartItems?.length > 0 && (
+        <>
+          <ScrollView>
+            {cartItems?.map((item) => (
+              <CartItem key={item._id} item={item} />
+            ))}
+          </ScrollView>
+          <View>
+            <PriceTable title="Price" price={999} />
+            <PriceTable title="Tax" price={1} />
+            <PriceTable title="Shipping" price={1} />
+            <View style={styles.grandTotal}>
+              <PriceTable title="Grand total" price={1001} />
             </View>
-          </>
-        )}
-      </View>
+            <TouchableOpacity
+              style={styles.checkoutBtn}
+              onPress={() => router.push("/checkout")}
+            >
+              <Text style={styles.checkoutBtnText}>Checkout</Text>
+            </TouchableOpacity>
+          </View>
+        </>
+      )}
     </View>
   );
 }
@@ -50,5 +63,19 @@ const styles = StyleSheet.create({
     padding: 5,
     margin: 5,
     marginHorizontal: 20,
+  },
+  checkoutBtn: {
+    marginTop: 20,
+    alignItems: "center",
+    justifyContent: "center",
+    height: 40,
+    backgroundColor: "#000",
+    width: "90%",
+    marginHorizontal: 20,
+    borderRadius: 20,
+  },
+  checkoutBtnText: {
+    color: "#fff",
+    fontWeight: "bold",
   },
 });
